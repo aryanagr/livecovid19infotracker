64ef4f71609c7d440430984b93f358003d602367
aryan-8947 <smartaryanagrawal987@gmail.com>
2021-07-24 09:51:16 +0530

    digital clock added as footer

---
+++ src/Time.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState } from 'react';
+const Time =()=>{
+    let newdate=new Date().toLocaleDateString();
+     let newtime=new Date().toLocaleTimeString();
+     const [ctime,setctime]=useState(newtime);
+     const [cdate,setcdate]=useState(newdate);
+     const updatedatetime=()=>{
+         setctime(new Date().toLocaleTimeString());
+         setcdate(new Date().toLocaleDateString());
+     }
+     setInterval(updatedatetime,1000);
+     setInterval(updatedatetime,1000);
+    return(
+    <footer class="page-footer font-small blue">
+
+
+        <div class="footer-copyright text-center py-3">
+           {cdate} {ctime}  
+           </div>
+
+
+    </footer>
+);
+}
+export default Time;
